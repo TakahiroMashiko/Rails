0161e290ddfef76b7873e255b324bc41c477aadd
EC2 Default User <ec2-user@ip-172-31-19-13.ec2.internal>
2021-03-21 03:39:04 +0000

    [Finish] 2-7

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -7,4 +7,5 @@
 #   )
 # end
 
+# uyuni_img_mv_pic01.jpg を追加
 Refile.secret_key = '98019a942be8b925df659dee30d942d30a3a1a6cc45202c956641724517b8ea23a3980f71acdb2f695676d9b07d7b81252092983a9ac59e5d5a09a2ef21e0f7b'
